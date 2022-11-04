b88823a837f6b5a2e005d086cca23a904c8777d0
Tong Zhang <zhangt@frib.msu.edu>
2022-11-04 08:59:54 -0400

    TST: Add test for a new .lpp file.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,8 +3,9 @@
 
 from lpp_parser import get_lpp_info
 
-lpp_file1 = '44S_target8mm.lpp'
-lpp_file2 = 'SetB.65.00-1H_E262_C15mm_36Ar__c_aris__PSv05_KFv3.5.0__CB1s-R4003.lpp'
+lpp_file1 = 'lppfiles/44S_target8mm.lpp'
+lpp_file2 = 'lppfiles/SetB.65.00-1H_E262_C15mm_36Ar__c_aris__PSv05_KFv3.5.0__CB1s-R4003.lpp'
+lpp_file3 = 'lppfiles/238U-190MeV-12C280mg-Alo85mm-223Th.lpp'
 
 
 def test_get_lpp_info_1():
@@ -37,3 +38,19 @@ def test_get_lpp_info_2():
         'tau': 0.26
     }
     assert settings['frag_beam'] == {'A': 1, 'name': 'H', 'Z': 1}
+
+
+def test_get_lpp_info_3():
+    d = get_lpp_info(lpp_file3)
+    settings = d['settings']
+    assert settings['primary_beam'] == {
+        'A': 238,
+        'name': 'U',
+        'Q': 92,
+        'Z': 92,
+        'Ek': 190.0,
+        'power': 5.0,
+        'rf_freq': 40.0,
+        'tau': 0.26
+    }
+    assert settings['frag_beam'] == {'A': 223, 'name': 'Th', 'Z': 90}
